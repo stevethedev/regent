22a0b84e27a90ab63d45bfc5b0f6319a5e755225
Steven Jimenez <stevenmjimenez@gmail.com>
2017-10-15 16:51:16 -0400

    Abstracts Routes
    This update further abstracts routes.

--- app/routes/web.js
+++ app/routes/web.js
@@ -6,6 +6,10 @@
 module.exports = function loadWebRoutes(router)
 {
     router.get('/', function(request, response) {
-        response.setBody('hello, world');
+        response.setBody('hello, world').send();
+    });
+
+    router.get('/test/{id}', function(request, response, { id }) {
+        response.setBody('Test: ' + id).send();
     });
 };
