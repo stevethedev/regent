5a91779c0e5a64511f862bcda58d383d3c464ba0
Steven Jimenez <stevenmjimenez@gmail.com>
2017-10-15 19:05:08 -0400

    adds testing suite for route objects

---
+++ bootstrap/system-config.js
@@ -0,0 +1,23 @@
+/**
+ * @author Steven Jimenez <steven@stevethedev.com>
+ */
+'use strict';
+
+const path            = require('path');
+const deepmerge       = require('deepmerge');
+
+const GlobalFunctions = require('./global-functions');
+const DefaultConfig   = require('../etc/default');
+const LocalConfig     = require('../etc/local');
+const SystemConfig    = deepmerge.all([{}, DefaultConfig, LocalConfig]);
+
+const rootDir = path.resolve(path.join(__dirname, '../'));
+GlobalFunctions.configure(rootDir, SystemConfig.Directories);
+
+module.exports = {
+    GlobalFunctions,
+    DefaultConfig,
+    LocalConfig,
+    SystemConfig,
+    rootDir,
+};
