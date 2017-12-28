d003b05d61be86fbb2514799d28b3ba45177d69a
Steven Jimenez <stevenmjimenez@gmail.com>
2017-12-28 00:20:48 -0500

    adds connection pool for PostgreSQL

--- test/lib/db/relational/postgresql/connection.js
+++ test/lib/db/relational/postgresql/connection.js
@@ -3,25 +3,75 @@
  */
 'use strict';
 
-const assert        = requireLib('util/assert');
-const PgConnection  = requireLib('db/relational/postgresql/connection');
+const assert     = requireLib('util/assert');
+const PostgresDb = requireLib('db/relational/postgresql/connection');
+const config     = require('./config');
 
-const CLASS_NAME    = PgConnection.name;
+const CLASS_NAME = PostgresDb.name;
+
+const connections = [];
+const getConnection = (options = config) => {
+    const connection = PostgresDb.create(options);
+    connections.push(connection);
+    return connection;
+};
 
 describe(`The ${CLASS_NAME} class`, () => {
     describe('constructor', () => {
         describe('(options) signature', () => {
-            it('should add a table prefix if options.prefix is set');
+            it('should add a table prefix if options.prefix is set', () => {
+                const prefix = 'my-prefix';
+                const psql = getConnection({ prefix });
+                assert.equal(psql.getPrefix(), prefix);
+            });
         });
     });
     describe('connect', () => {
         describe('() signature', () => {
-            it('should throw an error if connection fails');
-            it('should connect to the database');
-            it('should emit a "db-connect" event on success');
-            it('should not emit a "db-connect" event on failure');
-            it('should return a Promise');
-            it('should resolve to the connection object');
+            it('should return a Promise', () => {
+                const result = getConnection().connect();
+                assert.instanceOf(result, Promise);
+                return result;
+            });
+            it('should resolve to false if connection fails', () => {
+                const psql = getConnection({});
+                return psql.connect().then((result) => {
+                    psql.disconnect();
+                    assert.isFalse(result);
+                });
+            });
+            it('should resolve to true if connection succeeds', () => {
+                const psql = getConnection();
+                return psql.connect().then((result) => {
+                    psql.disconnect();
+                    assert.isTrue(result);
+                });
+            });
+            it('should emit a "db-connect" event on success', () => {
+                const psql = getConnection();
+                let eventCount = 0;
+                psql.on('db-connect', () => ++eventCount);
+                return psql.connect().then(() => {
+                    psql.disconnect();
+                    assert.equal(eventCount, 1);
+                });
+            });
+            it(
+                'should not emit a "db-connect" event on failure',
+                () => {
+                    const psql = getConnection();
+                    let eventCount = 0;
+                    psql.on('db-connect', () => ++eventCount);
+
+                    return Promise.resolve()
+                        .then(() => psql.connect())
+                        .then(() => psql.connect())
+                        .then(() => {
+                            psql.disconnect();
+                            assert.equal(eventCount, 1);
+                        });
+                }
+            );
         });
     });
     describe('disconnect', () => {
