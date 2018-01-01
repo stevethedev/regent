6f0d70a9a2797cba31d523385262162a0c546ded
Steven Jimenez <stevenmjimenez@gmail.com>
2018-01-01 18:15:56 -0500

    adds sorting on the QueryBuilder integration tests to prevent race-conditions and test multi-field insertions

--- test/integration/lib/db/relational/generic/query-builder.js
+++ test/integration/lib/db/relational/generic/query-builder.js
@@ -11,6 +11,7 @@ const Record       = requireLib('db/relational/record');
 const TABLE_NAME   = 'query_builder';
 const CLASS_NAME   = QueryBuilder.name;
 const COL_NAME     = 'test_col';
+const COL_ID       = 'id';
 const TABLE_VALUES = [
     'foo',
     'bar',
@@ -28,12 +29,18 @@ module.exports = function(testGroup, Connection, config) {
                     `DROP TABLE IF EXISTS ${TABLE_NAME}`
                 ))
                 .then(() => connection.send(
-                    `CREATE TABLE ${TABLE_NAME} (${COL_NAME} TEXT)`
+                    `CREATE TABLE ${TABLE_NAME} (
+                        ${COL_ID} INTEGER PRIMARY KEY,
+                        ${COL_NAME} TEXT
+                    )`
                 ))
                 .then(() => {
                     const query = connection.table(TABLE_NAME);
                     return Promise.all(TABLE_VALUES.map((value, i) => {
-                        return query.insert({ [COL_NAME]: value });
+                        return query.insert({
+                            [COL_ID]  : i + 1,
+                            [COL_NAME]: value,
+                        });
                     }));
                 });
         });
@@ -48,18 +55,18 @@ module.exports = function(testGroup, Connection, config) {
             describe('(<chunk-size>) signature', () => {
                 it('should return a generator-iterator', () => {
                     const query = connection.table(TABLE_NAME);
-                    return assert.isGenerator(query.chunk(1));
+                    return assert.isGenerator(query.orderBy(COL_ID).chunk(1));
                 });
                 it('should iterate into a Promise', () => {
                     const query   = connection.table(TABLE_NAME);
-                    const iter    = query.chunk(1);
+                    const iter    = query.orderBy(COL_ID).chunk(1);
                     const promise = iter.next().value;
                     assert.isPromise(promise);
                     return promise.then(() => iter.done());
                 });
                 it('should resolve into a Collection', () => {
                     const query = connection.table(TABLE_NAME);
-                    const iter  = query.chunk(1);
+                    const iter  = query.orderBy(COL_ID).chunk(1);
                     return iter.next().value
                         .then((result) => assert.instanceOf(
                             result,
@@ -70,7 +77,7 @@ module.exports = function(testGroup, Connection, config) {
                 it('should iterate through the entire result set', () => {
                     let promise = Promise.resolve();
                     const query = connection.table(TABLE_NAME);
-                    const iter  = query.chunk(1);
+                    const iter  = query.orderBy(COL_ID).chunk(1);
                     for (let i = 0; i < TABLE_VALUES.length; ++i) {
                         promise = promise.then(() => iter.next().value)
                             .then((rows) => {
@@ -87,7 +94,7 @@ module.exports = function(testGroup, Connection, config) {
                     'should return an empty array in the last result set',
                     () => {
                         const query = connection.table(TABLE_NAME);
-                        const iter = query.chunk(1);
+                        const iter = query.orderBy(COL_ID).chunk(1);
                         let promise = Promise.resolve();
                         for (let i = 0; i < TABLE_VALUES.length; ++i) {
                             promise = promise.then(() => iter.next().value);
@@ -108,7 +115,7 @@ module.exports = function(testGroup, Connection, config) {
                     (async () => {
                         const query = connection.table(TABLE_NAME);
                         let i = 0;
-                        for (const chunk of query.chunk(1)) {
+                        for (const chunk of query.orderBy(COL_ID).chunk(1)) {
                             assert.instanceOf(
                                 await chunk,
                                 Collection,
@@ -126,19 +133,19 @@ module.exports = function(testGroup, Connection, config) {
             describe('() signature', () => {
                 it('should return a Promise', () => {
                     const query = connection.table(TABLE_NAME);
-                    const promise = query.first();
+                    const promise = query.orderBy(COL_ID).first();
                     assert.isPromise(promise);
                     return promise;
                 });
                 it('should resolve to a Record', () => {
                     const query = connection.table(TABLE_NAME);
-                    const promise = query.first();
+                    const promise = query.orderBy(COL_ID).first();
                     return Promise.resolve(promise)
                         .then((record) => assert.instanceOf(record, Record));
                 });
                 it('should resolve to the first Record', () => {
                     const query   = connection.table(TABLE_NAME);
-                    const promise = query.first();
+                    const promise = query.orderBy(COL_ID).first();
                     return Promise.resolve(promise)
                         .then((record) => assert.equal(
                             record.getAttribute(COL_NAME),
@@ -151,13 +158,13 @@ module.exports = function(testGroup, Connection, config) {
             describe('() signature', () => {
                 it('should return a Promise', () => {
                     const query = connection.table(TABLE_NAME);
-                    const promise = query.get();
+                    const promise = query.orderBy(COL_ID).get();
                     assert.isPromise(promise);
                     return promise;
                 });
                 it('should resolve to a Collection', () => {
                     const query = connection.table(TABLE_NAME);
-                    const promise = query.get();
+                    const promise = query.orderBy(COL_ID).get();
                     return promise.then((collection) => assert.instanceOf(
                         collection,
                         Collection,
@@ -169,13 +176,13 @@ module.exports = function(testGroup, Connection, config) {
             describe('() signature', () => {
                 it('should return a generator-iterator', () => {
                     const query = connection.table(TABLE_NAME);
-                    const iter  = query.iterator();
+                    const iter  = query.orderBy(COL_ID).iterator();
                     assert.isGenerator(iter);
                     iter.done();
                 });
                 it('should iterate into Promises', () => {
                     const query = connection.table(TABLE_NAME);
-                    const iter  = query.iterator();
+                    const iter  = query.orderBy(COL_ID).iterator();
                     const promise = iter.next().value;
                     assert.isPromise(promise);
                     return Promise.resolve(promise)
@@ -183,7 +190,7 @@ module.exports = function(testGroup, Connection, config) {
                 });
                 it('should resolve into Records', () => {
                     const query = connection.table(TABLE_NAME);
-                    const iter  = query.iterator();
+                    const iter  = query.orderBy(COL_ID).iterator();
                     const promise = iter.next().value;
                     return Promise.resolve(promise)
                         .then((record) => {
@@ -198,19 +205,19 @@ module.exports = function(testGroup, Connection, config) {
             describe('(<field>) signature', () => {
                 it('should return a Promise', () => {
                     const query = connection.table(TABLE_NAME);
-                    const promise = query.last();
+                    const promise = query.orderBy(COL_ID).last();
                     assert.isPromise(promise);
                     return promise;
                 });
                 it('should resolve to a Record', () => {
                     const query = connection.table(TABLE_NAME);
-                    const promise = query.last();
+                    const promise = query.orderBy(COL_ID).last();
                     return Promise.resolve(promise)
                         .then((record) => assert.instanceOf(record, Record));
                 });
                 it('should resolve to the last Record', () => {
                     const query = connection.table(TABLE_NAME);
-                    const promise = query.last();
+                    const promise = query.orderBy(COL_ID).last();
                     return Promise.resolve(promise)
                         .then((record) => assert.equal(
                             record.getAttribute(COL_NAME),
@@ -223,13 +230,13 @@ module.exports = function(testGroup, Connection, config) {
             describe('(<field>) signature', () => {
                 it('should return a Promise', () => {
                     const query = connection.table(TABLE_NAME);
-                    const promise = query.pluck(COL_NAME);
+                    const promise = query.orderBy(COL_ID).pluck(COL_NAME);
                     assert.isPromise(promise);
                     return promise;
                 });
                 it('should resolve to a Collection', () => {
                     const query = connection.table(TABLE_NAME);
-                    const promise = query.pluck(COL_NAME);
+                    const promise = query.orderBy(COL_ID).pluck(COL_NAME);
                     return Promise.resolve(promise)
                         .then((collection) => assert.instanceOf(
                             collection,
@@ -240,7 +247,7 @@ module.exports = function(testGroup, Connection, config) {
                     'should only include the <field> key in the Collection',
                     () => {
                         const query = connection.table(TABLE_NAME);
-                        const promise = query.pluck(COL_NAME);
+                        const promise = query.orderBy(COL_ID).pluck(COL_NAME);
                         return Promise.resolve(promise)
                             .then((collection) => collection.forEach(
                                 (value, index) => {
@@ -255,13 +262,13 @@ module.exports = function(testGroup, Connection, config) {
             describe('(<field>) signature', () => {
                 it('should return a Promise', () => {
                     const query = connection.table(TABLE_NAME);
-                    const promise = query.value(COL_NAME);
+                    const promise = query.orderBy(COL_ID).value(COL_NAME);
                     assert.isPromise(promise);
                     return promise;
                 });
                 it('should resolve to the value in the first record', () => {
                     const query = connection.table(TABLE_NAME);
-                    const promise = query.value(COL_NAME);
+                    const promise = query.orderBy(COL_ID).value(COL_NAME);
                     return Promise.resolve(promise)
                         .then((value) => assert.equal(value, TABLE_VALUES[0]));
                 });
