468c6e601ca91c24f0bf92c33939abeb7ef11371
Steven Jimenez <stevenmjimenez@gmail.com>
2018-01-05 02:33:23 -0500

    completes the database object constructor

--- test/unit/lib/db/database.js
+++ test/unit/lib/db/database.js
@@ -3,10 +3,14 @@
  */
 'use strict';
 
-const assert   = require('regent/lib/util/assert');
-const Database = require('regent/lib/db/database');
+const assert         = require('regent/lib/util/assert');
+const Database       = require('regent/lib/db/database');
+const GenericDb      = require('regent/lib/db/relational/connection');
+const { $protected } = require('regent/lib/util/scope')();
 
-const CLASS_NAME   = Database.name;
+const CLASS_NAME     = Database.name;
+
+const DB_DRIVER      = 'GenericDb';
 
 const {
     DB_ACQUIRE,
@@ -36,35 +40,149 @@ const EVENT_ENUM = [
     DB_REMOVE,
 ];
 
+const { newRegent } = global;
+
+const regent       = newRegent();
+Database.registerDriver(DB_DRIVER, GenericDb);
+
 describe(`The ${CLASS_NAME} class`, () => {
+    describe('static registerDriver', () => {
+        describe('(<name>, <class>) signature', () => {
+            it('should register the driver class', () => {
+                assert.equal(
+                    Database.getDriverConstructor(DB_DRIVER),
+                    GenericDb
+                );
+            });
+        });
+    });
     describe('constructor', () => {
         describe('all signatures', () => {
             EVENT_ENUM.forEach((eventName) => {
                 it(`should register the ${eventName} event on Regent`);
             });
         });
-        describe('(regent, { read, write, options }) signature', () => {
-            it('should use different connections for read and write');
-            it('should fill in the "read" connection with "options"');
-            it('should fill in the "write" connection with "options"');
-            it('should prioritize the "read" connection over "options"');
-            it('should prioritize the "write" connection over "options"');
+        describe('(regent, driver, { read, write, options }) signature', () => {
+            const localRegent = newRegent();
+            const options     = {
+                bar: 'foo',
+                baz: 'foo',
+                foo: 'foo',
+            };
+            const read        = { bar: 'bar' };
+            const write       = { baz: 'baz' };
+            const settings    = {
+                options,
+                read,
+                write,
+            };
+            const database    = new Database(localRegent, DB_DRIVER, settings);
+            const $database   = $protected(database);
+            it('should use different connections for read and write', () => {
+                assert.notEqual(database.read(), database.write());
+            });
+            it('should fill in the "read" connection with "options"', () => {
+                assert.equal($database.options.read.foo, options.foo);
+            });
+            it('should fill in the "write" connection with "options"', () => {
+                assert.equal($database.options.write.foo, options.foo);
+            });
+            it('should prioritize the "read" connection over "options"', () => {
+                assert.equal($database.options.read.bar, read.bar);
+            });
+            it(
+                'should prioritize the "write" connection over "options"',
+                () => {
+                    assert.equal($database.options.write.baz, write.baz);
+                }
+            );
         });
-        describe('(regent, { read, options }) signature', () => {
-            it('should use different connections for read and write');
-            it('should fill in the "read" connection with "options"');
-            it('should use "options" for the "write" connection');
+        describe('(regent, driver, { read, options }) signature', () => {
+            const localRegent = newRegent();
+            const options     = {
+                bar: 'foo',
+                baz: 'foo',
+                foo: 'foo',
+            };
+            const read        = { bar: 'bar' };
+            const settings    = {
+                options,
+                read,
+            };
+            const database    = new Database(localRegent, DB_DRIVER, settings);
+            const $database   = $protected(database);
+            it('should use different connections for read and write', () => {
+                assert.notEqual(database.read(), database.write());
+            });
+            it('should fill in the "read" connection with "options"', () => {
+                assert.equal($database.options.read.foo, options.foo);
+            });
+            it('should prioritize the "read" connection over "options"', () => {
+                assert.equal($database.options.read.bar, read.bar);
+            });
+            it('should use "options" for the "write" connection', () => {
+                assert.equal($database.options.write.foo, options.foo);
+                assert.equal($database.options.write.bar, options.bar);
+                assert.equal($database.options.write.baz, options.baz);
+            });
         });
-        describe('(regent, { write, options }) signature', () => {
-            it('should use different connections for read and write');
-            it('should fill in the "read" connection with "options"');
-            it('should use "options" for the "read" connection');
+        describe('(regent, driver, { write, options }) signature', () => {
+            const localRegent = newRegent();
+            const options     = {
+                bar: 'foo',
+                baz: 'foo',
+                foo: 'foo',
+            };
+            const write       = { baz: 'baz' };
+            const settings    = {
+                options,
+                write,
+            };
+            const database    = new Database(localRegent, DB_DRIVER, settings);
+            const $database   = $protected(database);
+            it('should use different connections for read and write', () => {
+                assert.notEqual(database.read(), database.write());
+            });
+            it('should fill in the "write" connection with "options"', () => {
+                assert.equal($database.options.write.foo, options.foo);
+            });
+            it(
+                'should prioritize the "write" connection over "options"',
+                () => {
+                    assert.equal($database.options.write.baz, write.baz);
+                }
+            );
+            it('should use "options" for the "read" connection', () => {
+                assert.equal($database.options.read.foo, options.foo);
+                assert.equal($database.options.read.bar, options.bar);
+                assert.equal($database.options.read.baz, options.baz);
+            });
         });
-        describe('(regent, { read, write }) signature', () => {
-            it('should use different connections for read and write');
+        describe('(regent, driver, { read, write }) signature', () => {
+            const localRegent = newRegent();
+            const read        = { bar: 'bar' };
+            const write       = { baz: 'baz' };
+            const settings    = {
+                read,
+                write,
+            };
+            const database    = new Database(localRegent, DB_DRIVER, settings);
+            it('should use different connections for read and write', () => {
+                assert.notEqual(database.read(), database.write());
+            });
         });
-        describe('(regent, { options } signature', () => {
-            it('should use the same connection for read and write');
+        describe('(regent, driver, { options } signature', () => {
+            const localRegent = newRegent();
+            const options     = {
+                bar: 'foo',
+                baz: 'foo',
+                foo: 'foo',
+            };
+            const settings    = { options };
+            const database    = new Database(localRegent, DB_DRIVER, settings);
+            it('should use the same connection for read and write', () => {
+                assert.equal(database.read(), database.write());
+            });
         });
     });
     describe('select method', () => {
